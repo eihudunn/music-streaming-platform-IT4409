630e797eec3a681a1723b8af65c8636e83ed628d
nhv_21 <152309550+eihudunn@users.noreply.github.com>
2024-04-08 00:06:07 +0700

    add login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import './output.css';
+import './index.css';
+// import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginComponent from './routes/Login';
+import SignupComponent from './routes/Signup';
 
 function App() {
     return( 
@@ -9,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element= { <div>Hello</div> }/>
                 <Route path="/login" element={ <LoginComponent /> } />
+                <Route path="/signup" element={ <SignupComponent /> } />
             </Routes>
         </BrowserRouter>
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'gradient-to-b': 'linear-gradient(to bottom, var(--tw-gradient-stops))',
+      }),
+      colors: {
+        'signature-color': '#1ed760', // Example custom color
+      },
+    },
   },
   plugins: [],
 }
