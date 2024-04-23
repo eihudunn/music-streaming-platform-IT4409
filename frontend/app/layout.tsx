3e6834276dca8eafb0632556fc286fdb7c81ef54
pe4cE-hoa <hoayk0802@gmail.com>
2024-04-23 18:49:35 +0700

    modal

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Sidebar> 
           {children}
         </Sidebar>
---
+++ frontend/providers/UserProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+
+interface UserProviderProps {
+    children: React.ReactNode;
+}
+
+const UserProvider: React.FC<UserProviderProps> = ({children}) => {
+    return (
+        <></>
+    )
+};
+
+export default UserProvider;
