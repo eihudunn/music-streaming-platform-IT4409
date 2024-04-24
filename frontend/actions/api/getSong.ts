ff20d61e905c10e47d74174c1092f1eacb249169
nhv_21 <152309550+eihudunn@users.noreply.github.com>
2024-04-24 23:00:17 +0700

    get song fetch

---
+++ frontend/actions/api/getSong.ts
@@ -0,0 +1,59 @@
+import axiosClient from "@/app/_utils/GlobalApi";
+
+// const getSong = () => axiosClient.get('/song/get');
+
+const fakeGetSong = () => {
+    return [
+        {
+            id: "1",
+            title: "Bao tiền một mớ bình yên",
+            artist: "me",
+            href: "https://audio.https://res.cloudinary.com/dgpijywn0/video/upload/v1713953187/song/bao_ti%E1%BB%81n_m%E1%BB%99t_m%E1%BB%9B_b%C3%ACnh_y%C3%AAn-_Acoustic_-_14_Casper_Bon_Nghi%C3%AAm_-_B%C3%A0i_h%C3%A1t_lyrics_ulxrnd.mp3",
+            image: "https://res.cloudinary.com/dgpijywn0/image/upload/v1713953259/image/btmmby_b56lft.jpg"
+        },
+        {
+            id: "2",
+            title: "watafak",
+            artist: "me",
+            href: "https://res.cloudinary.com/dgpijywn0/video/upload/v1713953170/song/Tr%C3%B4i_-_Kha_-_B%C3%A0i_h%C3%A1t_lyrics_fxa13u.mp3",
+            image: "https://res.cloudinary.com/dgpijywn0/image/upload/v1713953259/image/btmmby_b56lft.jpg"
+        },
+        {
+            id: "3",
+            title: "How deep is your love",
+            artist: "me",
+            href: "https://res.cloudinary.com/dgpijywn0/video/upload/v1713953167/song/Gi%C3%B3_V%E1%BA%ABn_H%C3%A1t_-_Long_Ph%E1%BA%A1m_-_B%C3%A0i_h%C3%A1t_lyrics_c6puby.mp3",
+            image: "https://res.cloudinary.com/dgpijywn0/image/upload/v1713953259/image/btmmby_b56lft.jpg"
+        },
+        {
+            id: "4",
+            title: "Em chắc không",
+            artist: "me",
+            href: "https://res.cloudinary.com/dgpijywn0/video/upload/v1713953164/song/t%C3%ACm_-_Orange_-_B%C3%A0i_h%C3%A1t_lyrics_carx1i.mp3",
+            image: "https://res.cloudinary.com/dgpijywn0/image/upload/v1713953252/image/420070044_341327988811848_4196626351743886158_n_nziqww.jpg"
+        },
+        {
+            id: "5",
+            title: "Tình",
+            artist: "me",
+            href: "https://res.cloudinary.com/dgpijywn0/video/upload/v1713953150/song/Chuyen-Rang-Thinh-Suy_pjx9kr.mp3",
+            image: "https://res.cloudinary.com/dgpijywn0/image/upload/v1713953240/image/410947486_10219670237265490_8902717935161236416_n_te5lpx.jpg"
+        },
+        {
+            id: "6",
+            title: "Chuyện rằng",
+            artist: "me",
+            href: "https://res.cloudinary.com/dgpijywn0/video/upload/v1713953173/song/Em_Ch%E1%BA%AFc_Kh%C3%B4ng_-_Ph%E1%BA%A1m_Nguy%C3%AAn_Ng%E1%BB%8Dc_BMZ_-_B%C3%A0i_h%C3%A1t_lyrics_prcbtx.mp3",
+            image: "https://res.cloudinary.com/dgpijywn0/image/upload/v1713953259/image/btmmby_b56lft.jpg"
+        },
+        {
+            id: "7",
+            title: "How deep is your love",
+            artist: "me",
+            href: "https://res.cloudinary.com/dgpijywn0/video/upload/v1713953174/song/n%C3%A0ng_th%C6%A1..._tr%E1%BB%9Di_gi%E1%BA%A5u_tr%E1%BB%9Di_mang_%C4%91i_-_AMEE_Ho%C3%A0ng_D%C5%A9ng_-_B%C3%A0i_h%C3%A1t_lyrics_fxidtn.mp3",
+            image: "https://res.cloudinary.com/dgpijywn0/image/upload/v1713953259/image/nt_u6rqpl.jpg"
+        }
+    ]
+}
+
+export default fakeGetSong;
