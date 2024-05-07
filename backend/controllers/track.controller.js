8d6d0114a93cf21397da541e1f9a306e5b4c1140
HieuNT-2306 <hieubeo2369@gmail.com>
2024-05-07 13:13:08 +0700

    Test cloudinary

--- backend/controllers/track.controller.js
+++ backend/controllers/track.controller.js
@@ -4,7 +4,15 @@ const User = require('../schemas/user.js');
 const path = require('path');
 const fs = require('fs');
 const { model } = require('mongoose');
-const { tolowercaseVNese } = require('../helper/vietnameseTextToLowerCase.js');
+const { toLowerCaseNonAccentVietnamese } = require('../helper/vietnameseTextToLowerCase.js');
+const { search } = require('../app.js');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'degxdypyr',
+    api_key: 376626565986285,
+    api_secret: 'BIkyhrU0bwAVPMb0jONzz0mHx_8',
+});
 
 const getTracks = async (req, res) => {
     try {
@@ -17,30 +25,33 @@ const getTracks = async (req, res) => {
 
 const uploadTrack = async (req, res) => {
     try {
+        let hrefLink = '/track/' + req.file.filename;
+        console.log(hrefLink);
+        const uploadResult = await cloudinary.uploader.upload(hrefLink, { resource_type: 'raw' });
+        console.log(uploadResult);
         let track = new Track({
             title: req.body.title,
-            artist: req.body.artist,    
-            searchTitle: req.body.title.tolowercaseVNese(),
-            href: '/track/' + req.file.filename,
+            searchTitle: toLowerCaseNonAccentVietnamese(req.body.title),
+            artist: req.body.artist,
+            href: uploadResult.secure_url,
             album: req.body.album,
             genre: req.body.genre,
             plays: 0,
             likes: 0,
-            comments: []
+            comments: [],
         });
-
+        console.log(track);
         track.save(function (err) {
             if (err) {
-                res.status(500).json({ message: err.message });
-            } else {
-                res.status(200).json({ message: 'Track uploaded successfully!', track: track });
+                return res.status(500).json({ message: err.message });
             }
+            res.status(200).json({ message: 'Track uploaded successfully!', track });
         });
-    }
-    catch (error) {
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
+
 const updateTrack = async (req, res) => {
     try {
         const track = await Track.findById(req.params.id);
@@ -74,15 +85,10 @@ const deleteTrack = async (req, res) => {
         if (!track) {
             return res.status(404).json({ message: 'Track not found' });
         }
+        const publicId = track.href.split('/').pop();
+        await cloudinary.uploader.destroy(publicId);
         await Track.findByIdAndRemove(req.params.id);
-        fs.unlink(path.join(__dirname, '..', 'track', path.basename(track.href)), err => {
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ message: err.message });
-            }
-
-            res.json({ message: 'Track deleted successfully!' });
-        });
+        res.json({ message: 'Track deleted successfully!' });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
--- backend/routes/track.js
+++ backend/routes/track.js
@@ -14,7 +14,6 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage })
 
-
 router.get("/get", getTracks);
 router.post("/post",upload.single('song') ,uploadTrack);
 router.delete("/delete/:id", deleteTrack);
