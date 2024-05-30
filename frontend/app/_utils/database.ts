53d12e38b40adc7e6ff42815e1c8e261e5dd7283
pe4cE-hoa <hoayk0802@gmail.com>
2024-05-30 20:39:26 +0700

    "add configuration button"

--- frontend/app/_utils/database.ts
+++ frontend/app/_utils/database.ts
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || "";
+    const mongoURI = process.env.MONGO_URI || '';
     await mongoose.connect(mongoURI, {
       dbName: process.env.MONGO_DB,
     });
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
+    console.error('Failed to connect to MongoDB:', error);
   }
 };
